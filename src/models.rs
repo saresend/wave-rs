4653dd67c907bf1b7fb13637a90d2310e52efb14
saresend <saresend@usc.edu>
2018-04-01 19:54:56 -0700

    now makes sounds

--- src/models.rs
+++ src/models.rs
@@ -28,6 +28,14 @@ impl Node {
         let event_loop_1 = cpal::EventLoop::new();
         let event_loop_2 = cpal::EventLoop::new();
 
+        let sample_rate = format.sample_rate.0 as f32;
+        let mut sample_clock = 0f32;
+
+        let mut next_value = move || {
+            sample_clock = (sample_clock + 1.0) % sample_rate;
+            (sample_clock * 440.0 * 2.0 * 3.141592 / sample_rate).sin()
+        };
+
         let stream_id = event_loop_1.build_output_stream(&device, &format).unwrap();
         event_loop_1.play_stream(stream_id);
         thread::spawn(move || 
@@ -38,20 +46,25 @@ impl Node {
                 //TODO: Fix, this feels like a bad solution to cancel node execution
             }
             else {
+               
                 match stream_data {
+                    
                     StreamData::Output { buffer: UnknownTypeOutputBuffer::U16(mut buffer) } => {
+                        let val = next_value();
                         for elem in buffer.iter_mut() {
-                            *elem = u16::max_value() / 2;
+                            *elem = val as u16;
                         }
                     },
                     StreamData::Output { buffer: UnknownTypeOutputBuffer::I16(mut buffer) } => {
+                        let val = next_value();
                         for elem in buffer.iter_mut() {
-                            *elem = 0;
+                            *elem = val as i16;
                         }
                     },
                     StreamData::Output { buffer: UnknownTypeOutputBuffer::F32(mut buffer) } => {
+                        let val = next_value();
                         for elem in buffer.iter_mut() {
-                            *elem = 0.0;
+                            *elem = val;
                         }
                     },
                     _ => (),
--- tests/node_test.rs
+++ tests/node_test.rs
@@ -1,7 +1,9 @@
 extern crate wave_rs;
-
+use std::thread;
+use std::time::Duration;
 #[test]
 fn test_initialize() {
     let node = wave_rs::models::Node::new();
     node.initialize();
+    thread::sleep(Duration::new(5, 0));
 }
